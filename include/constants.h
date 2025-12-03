eea17595239bd2a9cf08432584efa54da4cfab65
Minh Nhat Nguyen <minhnhatnguyen.aug@gmail.com>
2025-12-03 23:42:41 +0700

    Add layer and tensor classes for GPU and CPU operations

--- include/constants.h
+++ include/constants.h
@@ -1,4 +1,8 @@
+#pragma once
 constexpr int KERNEL_WIDTH = 3;
 constexpr int KERNEL_RADIUS = KERNEL_WIDTH / 2;
 constexpr int KERNEL_SIZE = KERNEL_WIDTH * KERNEL_WIDTH;
 constexpr int BLOCK_SIZE = 16;
+constexpr int IMAGE_DIMENSION = 32; 
+constexpr int IMAGE_PIXELS = IMAGE_DIMENSION * IMAGE_DIMENSION;
+constexpr size_t IMAGE_BYTE_SIZE = sizeof(float) * 3 * IMAGE_PIXELS;
