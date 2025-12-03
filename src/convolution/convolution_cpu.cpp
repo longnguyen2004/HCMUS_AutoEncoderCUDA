d303ab1b5a44bc9b1f37d326f4484bfe9556e184
nghiaphucnguyen <nghiaphucnguye@gmail.com>
2025-12-03 18:12:12 +0700

    tensorflow-like padding

--- src/convolution/convolution_cpu.cpp
+++ src/convolution/convolution_cpu.cpp
@@ -12,9 +12,12 @@ void convolve_cpu(float* dst, float* src, float* kernel, int row, int col)
       {
         for (int l = 0; l < KERNEL_WIDTH; ++l)
         {
-          int i_mapped = std::clamp(i + k - KERNEL_RADIUS, 0, row - 1);
-          int j_mapped = std::clamp(j + l - KERNEL_RADIUS, 0, col - 1);
-          val += src[i_mapped * col + j_mapped] * kernel[k * KERNEL_WIDTH + l];
+          int i_mapped = i + k - KERNEL_RADIUS;
+          int j_mapped = j + l - KERNEL_RADIUS;
+          
+          if (i_mapped >= 0 && i_mapped < row && j_mapped >= 0 && j_mapped < col) {
+             val += src[i_mapped * col + j_mapped] * kernel[k * KERNEL_WIDTH + l];
+          }
         }
       }
       dst[i * col + j] = val;
