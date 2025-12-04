22225064c6949b76d3677687e7c8571ff7b9a711
Long Nguyen <nguyen.long.908132@gmail.com>
2025-12-04 14:55:42 +0700

    Remove unused files, clear out main for preparation of actual code

--- src/convolution/gpu/convolution_gpu.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "convolution/base/convolution_base.h"
-
-class ConvolutionGpu : public Convolution {
-public: 
-  void convolve(
-    float *dst, float *src, float *kernel, int row, int col) override;
-};
