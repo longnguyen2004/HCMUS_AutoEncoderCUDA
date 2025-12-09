22ac8287729d27e0188ff8ae14ab1d2dffcfcfbe
Long Nguyen <nguyen.long.908132@gmail.com>
2025-12-09 22:58:27 +0700

    Convert CIFAR images to float

--- src/core/cifar_reader.h
+++ src/core/cifar_reader.h
@@ -7,20 +7,23 @@
 struct Image
 {
   uint8_t label;
-  std::vector<uint8_t> data;
+  std::vector<float> data;
 };
 
 std::vector<Image> read_cifar10(std::istream& in)
 {
   std::vector<Image> images;
+  uint8_t image_u8[3072];
   while (true)
   {
     Image image;
     if (!in.read(reinterpret_cast<char*>(&image.label), 1))
       break;
     image.data.resize(3072);
-    if (!in.read(reinterpret_cast<char*>(image.data.data()), 3072))
+    if (!in.read(reinterpret_cast<char*>(image_u8), 3072))
       break;
+    for (int i = 0; i < 3072; i++)
+      image.data[i] = image_u8[i] / 255.0f;
     images.push_back(std::move(image));
   }
   return images;
